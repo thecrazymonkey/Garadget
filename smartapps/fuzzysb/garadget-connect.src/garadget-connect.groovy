e39a4d8c49d00ecbe33eaf4e1666ca96c58fdaf9
Ivan Kunz <ivan.kunz@openet.us>
2018-02-02 19:00:51 -0500

    added more code

--- smartapps/fuzzysb/garadget-connect.src/garadget-connect.groovy
+++ smartapps/fuzzysb/garadget-connect.src/garadget-connect.groovy
@@ -254,6 +254,8 @@ def receivedToken() {
 def doorStatus() {
     def requestJSON = request.JSON
     log.debug "Reported status json: $requestJSON"
+    def paramsJSON = params.JSON
+    log.debug "Reported params status json: $paramsJSON"
 }
 
 
